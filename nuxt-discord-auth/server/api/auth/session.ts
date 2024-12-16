48c0f09bcc6e6a75f32efcd28676bf2ba2e63988
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-16 17:38:51 +0700

    Add API session code to retrieve Session data (add code for actual use).

--- nuxt-discord-auth/server/api/auth/session.ts
+++ nuxt-discord-auth/server/api/auth/session.ts
@@ -1,7 +1,15 @@
-import { defineEventHandler } from "h3";
+import { defineEventHandler, getCookie } from "h3";
+import jwt from "jsonwebtoken"
+import { JWT_SECRET } from "~/server/config/auth";
 
 export default defineEventHandler((event) => {
-    return {
-        message: "session auth"
+    const token = getCookie(event, 'session');
+    if (!token) return { session: null };
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        return { session: decoded };
+    } catch (error) {
+        return { session: null }
     }
-})
+});
