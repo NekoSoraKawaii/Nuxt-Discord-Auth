bbb4e42dd17e4650da783a8b84537b63f0677589
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-16 18:17:59 +0700

    Add the basic code for how to use the useDiscordAuth function.

--- nuxt-discord-auth/app.vue
+++ nuxt-discord-auth/app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
     <h1>Nuxt 3 Discord Auth</h1>
+
+    <div v-if="session">
+      <p>Welcome, {{ session.username }}</p>
+    </div>
+    <div v-else>
+      <button @click="signIn">Sign In</button>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import useDiscordAuth from './composables/useDiscordAuth';
+
+const { signIn, session, getSession } = useDiscordAuth();
+
+onMounted(() => {
+  getSession();
+})
+</script>
