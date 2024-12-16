072795cf9c031cd1f942fb757e4ff504b39e4b06
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-16 16:50:13 +0700

    Add a callback code for real use and adjust to have the base URI in the .env file instead of hardcoding it in the source code.

--- nuxt-discord-auth/server/api/auth/callback.ts
+++ nuxt-discord-auth/server/api/auth/callback.ts
@@ -1,7 +1,42 @@
-import { defineEventHandler } from "h3"
+import { defineEventHandler, setCookie, getQuery } from "h3";
+import jwt from "jsonwebtoken";
+import axios from "axios"
+import { DISCORD_CONFIG, JWT_SECRET } from "~/server/config/auth";
 
-export default defineEventHandler((event) => {
-    return {
-        message: "callback auth"
+export default defineEventHandler(async (event) => {
+    const { code } = getQuery(event);
+
+    if (!code) {
+        throw new Error("Authorization code missing")
+    }
+
+    try {
+        const tokenResponse = await axios.post(`${process.env.BASE_URI_API}/api/oauth2/token`, new URLSearchParams({
+            client_id: DISCORD_CONFIG.clientId,
+            client_secret: DISCORD_CONFIG.clientSecret,
+            grant_type: "authorization_code",
+            code: code as string,
+            redirect_uri: DISCORD_CONFIG.redirectUri
+        }).toString(), {
+            headers: { "Content-Type": "application/x-www-form-urlencoded" }
+        });
+
+        const { access_token } = tokenResponse.data;
+        const userResponse = await axios.get(`${process.env.BASE_URI_API}/api/users/@me`, {
+            headers: { Authorization: `Bearer ${access_token}` }
+        });
+
+        const user = userResponse.data;
+        const token = jwt.sign({ user }, JWT_SECRET, { expiresIn: "1h" });
+        setCookie(event, "session", token, {
+            httpOnly: true,
+            maxAge: 3600,
+            path: "/"
+        });
+
+        return { success: true };
+    } catch (error) {
+        console.error(error);
+        throw new Error("OAuth2 Failed")
     }
 })
--- nuxt-discord-auth/server/api/auth/login.ts
+++ nuxt-discord-auth/server/api/auth/login.ts
@@ -3,7 +3,7 @@ import { DISCORD_CONFIG } from "~/server/config/auth";
 
 export default defineEventHandler((event) => {
     const scope = encodeURIComponent('identify email');
-    const discordAuthUrl = `https://discord.com/oauth2/authorize?client_id=${DISCORD_CONFIG.clientId}&redirect_uri=${encodeURIComponent(DISCORD_CONFIG.redirectUri)}&response_type=code&scope=${scope}`
+    const discordAuthUrl = `${process.env.BASE_URI_API}/oauth2/authorize?client_id=${DISCORD_CONFIG.clientId}&redirect_uri=${encodeURIComponent(DISCORD_CONFIG.redirectUri)}&response_type=code&scope=${scope}`
 
     return sendRedirect(event, discordAuthUrl);
 })
