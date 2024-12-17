df73371501bbd74d0b9fb26f8e4b7c2f351bf7c5
NekoSakuraLucia <mokurolucia@gmail.com>
2024-12-17 16:46:35 +0700

    Add an index page as the homepage for the website.

---
+++ nuxt-discord-auth/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="h-screen bg-neutral-900 text-white">
+        <div class="h-full flex flex-col items-center justify-center text-center">
+            <div class="font-bold mb-5">
+                <h3 class="text-4xl md:text-5xl text-transparent bg-clip-text bg-gradient-to-r from-[#00C58E] to-[#7C3AED] mb-2">
+                    Nuxt 3 -
+                    Discord Auth</h3>
+                <p class="text-sm text-transparent bg-clip-text bg-gradient-to-r from-[#00C58E] to-[#35495E]">This project demonstrates how to implement Discord OAuth2
+                    Authentication in a Nuxt 3 application. <br /> It allows users to sign in via Discord, fetch their
+                    profile data, and manage authentication sessions seamlessly.</p>
+            </div>
+            <div class="font-semibold rounded-lg p-2 px-8 bg-gradient-to-r from-[#00C58E] to-[#35495E]">
+                <a href="/">Go to Profile Page</a>
+            </div>
+        </div>
+    </div>
+</template>
